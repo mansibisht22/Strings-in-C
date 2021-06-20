27d10990e17bb682348897fa6c5e83f684d7236f
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-06-20 11:03:03 +0530

    Add files via upload

---
+++ 1-D strings/string1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+
+    char str1, str2, str3, str4;
+
+    printf("Enter the string: ");
+    char str[25];
+    gets(str);
+    puts(str);
+}
