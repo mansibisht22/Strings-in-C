3e0e9aff1ff81b2bc65fd358a51ed41a509ff388
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-07-11 09:54:37 +0530

    Add files via upload

---
+++ string3.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char str[100];
+	int i, num, fin = 0;
+	printf("Enter a string of numbers : ");
+	gets(str);
+	for (i = 0; str[i] != '\0'; i++)/*Run until string is not terminated.*/
+	{
+		num = str[i] - 48;/*Converting the character number into integer number.*/
+		fin = fin * 10 + num;/*Combining the individual numbers to form a single integers*/
+	}
+	printf("\nNumber : %d", fin);
+	return 0;
+
+}
