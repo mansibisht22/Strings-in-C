b7014f1c4a6e963cde6476ba6d277fcdcfd501e5
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-07-16 07:57:36 +0530

    Add files via upload

---
+++ string8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+
+    int i,j;
+    
+    struct marks{
+        int j[3];
+        int marks;
+    };
+
+    struct marks play[5];
+
+    for(i=0; i<5; i++){
+        for(j=0; j<3; j++){
+            printf("Enter marks for player %d: ", i+1);
+            scanf("%d", &marks[i])
+        
+        }
+    }
+
+
+}
